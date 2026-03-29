79ac479aaf419e7c762b0a88857dadca3726d940
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-03-29 09:20:50 -0400

    Create NavDisplay.kt

---
+++ composeApp/src/webMain/kotlin/com/example/webcomposetest/navigation/NavDisplay.kt
@@ -0,0 +1,59 @@
+package com.example.webcomposetest.navigation
+
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.unit.IntOffset
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
+import androidx.navigation3.ui.NavDisplay
+
+@Composable
+fun NavDisplay(backStack: SnapshotStateList<NavKey>) {
+    val animationSpec = tween<IntOffset>(500)
+
+    NavDisplay(
+        backStack = backStack,
+        onBack = { backStack.removeLastOrNull() },
+        entryDecorators = listOf(
+            rememberSaveableStateHolderNavEntryDecorator(),
+            rememberViewModelStoreNavEntryDecorator()
+        ),
+        entryProvider = entryProvider {
+            StudentListEntry(backStack)
+            StudentDetailEntry(backStack)
+        },
+        transitionSpec = {
+            slideInHorizontally(
+                initialOffsetX = { it },
+                animationSpec = animationSpec
+            ) togetherWith slideOutHorizontally(
+                targetOffsetX = { -it },
+                animationSpec = animationSpec
+            )
+        },
+        popTransitionSpec = {
+            slideInHorizontally(
+                initialOffsetX = { -it },
+                animationSpec = animationSpec
+            ) togetherWith slideOutHorizontally(
+                targetOffsetX = { it },
+                animationSpec = animationSpec
+            )
+        },
+        predictivePopTransitionSpec = {
+            slideInHorizontally(
+                initialOffsetX = { -it },
+                animationSpec = animationSpec
+            ) togetherWith slideOutHorizontally(
+                targetOffsetX = { it },
+                animationSpec = animationSpec
+            )
+        }
+    )
+}
