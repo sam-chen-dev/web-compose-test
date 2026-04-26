9d659508b05bb794c7376fe1668484f9975a2cf5
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-04-26 19:56:46 -0400

    Create OrderListScreen.kt

---
+++ composeApp/src/webMain/kotlin/com/example/webcomposetest/features/orderList/OrderListScreen.kt
@@ -0,0 +1,34 @@
+package com.example.webcomposetest.features.orderList
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun OrderListScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Toolbar()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Toolbar() {
+    TopAppBar(
+        title = { Text("Order List") },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    )
+}
