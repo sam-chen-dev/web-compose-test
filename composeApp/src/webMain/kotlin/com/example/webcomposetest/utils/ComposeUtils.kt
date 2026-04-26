b98874df1b78d634280961a011f2a9adba5fd63c
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-04-26 19:44:25 -0400

    Update ComposeUtils.kt

--- composeApp/src/webMain/kotlin/com/example/webcomposetest/utils/ComposeUtils.kt
+++ composeApp/src/webMain/kotlin/com/example/webcomposetest/utils/ComposeUtils.kt
@@ -1,5 +1,6 @@
 package com.example.webcomposetest.utils
 
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Button
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.FilledTonalIconButton
@@ -8,9 +9,12 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.TextUnit
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.StringResource
@@ -25,6 +29,30 @@ fun Text(text: String, color: Color = Color.Unspecified, fontSize: TextUnit = Te
 @Composable
 fun Text(res: StringResource, color: Color = Color.Unspecified, fontSize: TextUnit = TextUnit.Unspecified) {
     Text(stringResource(res), color = color, fontSize = fontSize)
+}
+
+@Composable
+fun TextButton(res: StringResource, onClick: () -> Unit) {
+    TextButton(onClick) {
+        Text(res)
+    }
+}
+
+@Composable
+fun TextButton(text: String, onClick: () -> Unit) {
+    TextButton(onClick) {
+        Text(text)
+    }
+}
+
+@Composable
+fun Icon(
+    imageVector: ImageVector,
+    contentDescription: String,
+    tint: Color = LocalContentColor.current,
+    modifier: Modifier = Modifier
+) {
+    Icon(imageVector, contentDescription, tint = tint, modifier = modifier)
 }
 
 @Composable
@@ -99,6 +127,16 @@ fun Button(text: String, onClick: () -> Unit) {
     Button(onClick) {
         Text(text)
     }
+}
+
+@Composable
+fun Image(
+    res: DrawableResource,
+    contentDescription: String,
+    contentScale: ContentScale,
+    modifier: Modifier = Modifier
+) {
+    Image(painterResource(res), contentDescription, contentScale = contentScale, modifier = modifier)
 }
 
 /*
