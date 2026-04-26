71ec8638eb7df1dc4e67bee9afc4769ed8c690ab
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-04-26 19:43:04 -0400

    Create DateTimeFormat.kt

---
+++ composeApp/src/webMain/kotlin/com/example/webcomposetest/utils/DateTimeFormat.kt
@@ -0,0 +1,13 @@
+package com.example.webcomposetest.utils
+
+object DateTimeFormat {
+    const val MM_DD_YYYY_HYPHEN = "MM-dd-yyyy"
+    const val MM_DD_YYYY_HH_MM_SS_HYPHEN = "MM-dd-yyyy, HH:mm:ss"
+    const val MM_DD_YYYY_HH_MM_SS_SSS_HYPHEN = "MM-dd-yyyy, HH:mm:ss:SSS"
+
+    const val MM_DD_YYYY_SLASH = "MM/dd/yyyy"
+    const val MM_DD_YYYY_HH_MM_SS_SLASH = "MM/dd/yyyy, HH:mm:ss"
+    const val MM_DD_YYYY_HH_MM_SS_SSS_SLASH = "MM/dd/yyyy, HH:mm:ss:SSS"
+
+    const val H_MM_12 = "h:mm a"
+}
