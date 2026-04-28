9482c2b40f6758693077908881622c4d5d84c56e
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-04-28 16:11:58 -0400

    Add: Constants for Stripe Keys

---
+++ composeApp/src/webMain/kotlin/com/example/webcomposetest/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.webcomposetest.utils
+
+object Constants {
+    /*Stripe Publishable Keys*/
+    const val GOOGLE_PAY_MERCHANT_ID = "BCR2DN5T5376ZNKG"
+    const val MERCHANT_NAME = "Sam Chen Dev"
+    const val STRIPE_TEST_PUBLISHABLE_KEY =
+        "pk_test_51Swy3EI0dpttvRQeuyMff5ZhbWfw6DMSPnjyeNbknbRTQkVK7CK7IPpKWDUx758wn68rtQpbM3PbV2riQCerhGE300iXW0Df5C"
+    const val STRIPE_PRODUCTION_PUBLISHABLE_KEY =
+        "pk_live_51Swy3EI0dpttvRQeyJIKSLpsfZBudEbKYAi44i7sKE2W0UzlQwALSVga2MO574JIDGHOU1P5LHSsYOyCOkI2X5mO00AoYVAkVM"
+}
